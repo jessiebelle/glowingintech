6af732f0a80f0f674e2bdcc87ab30b0e28699c30
jessiebelle <jessieauguste@gmail.com>
2022-12-29 13:45:45 +0000

    adding files

--- frontend/src/homepage/components/headerimage.jsx
+++ frontend/src/homepage/components/headerimage.jsx
@@ -3,17 +3,7 @@ import styles from "./Picture.module.css";
 import image from "../../static/ctrlimg.png";
 import {Button, createTheme, ThemeProvider} from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#0A2A5E',
-    },
-    secondary: {
-      main: '#F15C80',
-      textColorPrimary: '#00AEEF'
-    },
-  },
-});
+
 const HeaderImage = () => {
   return (
     <article className={styles.article}>
@@ -22,9 +12,6 @@ const HeaderImage = () => {
         <img src={image} alt="Amber and Jessie in the studio" className={styles.picture} />
       </picture>
       <h1 className={styles.header}>Empowering techies to succeed and thrive through podcasts, resources, and community.</h1>
-      <ThemeProvider theme={theme}>
-        <Button color="secondary" variant="contained" size="large" classes={styles.button}>Join the community</Button>
-        </ThemeProvider>
 
     </article>
   );
