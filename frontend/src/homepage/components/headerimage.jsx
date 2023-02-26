815f3d77f4ae38b534122a678a52d81fc90e6896
jessiebelle <jessieauguste@gmail.com>
2023-02-26 21:01:39 +0000

    lower header img quality

--- frontend/src/homepage/components/headerimage.jsx
+++ frontend/src/homepage/components/headerimage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import styles from "./Picture.module.css";
-import image from "../../static/Untitled.png";
+import image from "../../static/header.jpg";
 
 
 const HeaderImage = () => {
