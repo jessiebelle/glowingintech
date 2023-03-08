7e4ef283cd81c58fe699d1be3b85f301b6f94e08
jessiebelle <jessieauguste@gmail.com>
2023-03-08 20:48:21 +0000

    adding backend

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-eiq$c2u%&rl&w-f39hgl55z@yy)=e9wi+qbak-dq$hn5(p&45m"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
