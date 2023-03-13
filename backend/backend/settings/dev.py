742e6eaea4dbe9370b837e8499bb427357cc20b7
jessiebelle <jessieauguste@gmail.com>
2023-03-13 15:30:19 +0000

    updating for aws

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -4,6 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY", "SUPER_SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
