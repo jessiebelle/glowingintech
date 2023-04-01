b24b23c381c14aa24aa0d8c82e38165e65fc6e20
Jessie <jessieauguste@gmail.com>
2023-04-01 13:59:54 +0100

    adding some resources to resources page

---
+++ frontend/src/resources/components/EpisodeShownotes.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import GITSubstack from '../../static/GITSubstack.jpg';
+import ctrlImage from '../../static/ctrlimg.png';
+
+export default function EpisodeShownotes() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea href={"https://glowing-in-tech.notion.site/Glowing-in-Tech-Show-notes-f568e988a20b40c5bde611fa5e18e234"}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={ctrlImage}
+          alt="Jessie and Amber in the podcast studio"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Glowing in Tech Podcast Shownotes
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+Information from all released episodes, resources mentioned by guests and their details          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
---
+++ frontend/src/resources/components/GITCard.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import GITSubstack from '../../static/GITSubstack.jpg';
+
+export default function GITCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea href={"https://glowingintech.substack.com/"}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={GITSubstack}
+          alt="Jessie on stage at LeadDev conference"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Glowing in Tech Substack
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+Resources for getting into tech, guides and information on our guests and their topics          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
